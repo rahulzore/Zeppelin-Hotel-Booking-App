709f3b3f36bc736b6e3a72af26d7a4132d8fac33
Rahul Zore <zore.r@husky.neu.edu>
2018-07-13 20:08:39 -0400

    added manage rentals and bookings endpoint, added delete rental endpoint

--- server/controller/booking.js
+++ server/controller/booking.js
@@ -48,6 +48,20 @@ exports.createBooking = function(req, res) {
 exports.getUserBookings = function(req, res) {
   const user = res.locals.user;
 
+    Booking.where({user})
+    .populate('rental')
+    .exec(function(err, foundBookings){
+        if(err){
+            return res.status(422).send({ errors: normalizeErrors(err.errors) });
+        }
+
+        return res.json(foundBookings)
+    })
+}
+
+exports.getUserBookings = function(req, res) {
+  const user = res.locals.user;
+
   Booking
     .where({user})
     .populate('rental')
